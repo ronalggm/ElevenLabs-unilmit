65ff8b16077c1ece7907aee2791f8748e5f12566
totone <ronalggm@gmail.com>
2025-01-06 23:23:49 -0300

    creando metodo splitVideoIn270s

--- src/main/java/common/videoSplitter/VideoSplitter.java
+++ src/main/java/common/videoSplitter/VideoSplitter.java
@@ -53,15 +53,7 @@ public class VideoSplitter {
                 FFmpegExecutor executor = new FFmpegExecutor(ffmpeg);  //executor sirve para ejectuar el builder
 
                 executor.createJob(builder).run();
-                // executor.createTwoPassJob(builder).run();
-                //.setAudioChannels(2)
-                // .setAudioCodec("aac")
-                //.setAudioSampleRate(48_000)
-                //.setAudioBitRate(128 * 1024)
-                //.setVideoCodec("libx264")
-                //.setVideoBitRate(4000 * 1024)
-                //.setVideoFrameRate(30, 1)
-                // .setVideoResolution(1920, 1080)
+
                 System.out.println("Codificacion completada exitosamente");
             } catch (IOException e) {
                 System.err.println("Error de " + e.getMessage());
@@ -73,6 +65,68 @@ public class VideoSplitter {
 
 
     }
+
+
+    public static void splitVideoIn270s(File selectedVideo) throws IOException {
+        FFmpeg ffmpeg = new FFmpeg("/usr/bin/ffmpeg"); //define la ubicacion de FFmpeg
+        FFprobe ffprobe = new FFprobe("/usr/bin/ffprobe"); //define ubicacion de FFprobe
+
+//probe selecciona el video, toma el tiempo y luego lo formateamos a la duracion en  double
+        double totalDuration =
+                ffprobe.probe(selectedVideo.getAbsolutePath())
+                        .getFormat().duration;
+        System.out.println("Video duration: " + totalDuration);
+        //Calcular la cantidad de segmentos completos y el tiempo restante
+        int segmentDuration = 270; //4,30minutos
+        int fullSegments = (int) (totalDuration / segmentDuration);
+        double remainingTime = totalDuration % segmentDuration;
+        int cutPosition;
+
+        for (int i = 1; i <= fullSegments; i++) {
+            cutPosition =fullSegments*i;
+            if(i==fullSegments && remainingTime>0){
+
+
+            }
+            try {
+                //define el archivo de salida y su nombre
+                String outputFilename = new File(selectedVideo.getParent(),
+                        selectedVideo.getName()
+                                .replace(".mp4", "")
+                                + "_part_"
+                                + i
+                                + ".mp4")
+                        .getAbsolutePath();
+
+
+                FFmpegBuilder builder = new FFmpegBuilder()
+                        .setInput(selectedVideo.getAbsolutePath())
+                        .overrideOutputFiles(true)
+                        .addOutput(outputFilename) //establece el nombre del archivo y la salida
+                        .setStartOffset((long) cutposition, TimeUnit.SECONDS)
+                        .setDuration((long) partDuration, TimeUnit.SECONDS) //duracion de cada segmento(desde el corte hasta el tiemnpo definido)
+                        .setFormat("mp4")
+                        .addExtraArgs("-c", "copy")// copia los parametros del video, resolucion, bitrate etc
+                        .done()
+                        .setVerbosity(FFmpegBuilder.Verbosity.DEBUG);
+
+                FFmpegExecutor executor = new FFmpegExecutor(ffmpeg);  //executor sirve para ejectuar el builder
+
+                executor.createJob(builder).run();
+
+                System.out.println("Codificacion completada exitosamente");
+            } catch (IOException e) {
+                System.err.println("Error de " + e.getMessage());
+                e.printStackTrace();
+
+            }
+
+        }
+
+
+    }
+
+
 }
 
 
